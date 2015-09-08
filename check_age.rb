92b77ea85c8e52509cfb5beac411fe19d85d168b
justin richards <richards.justind@gmail.com>
2015-09-08 16:56:27 -0500

    punctuation

--- check_age.rb
+++ check_age.rb
@@ -1,11 +1,11 @@
 def uk_rules(x)
-  puts "You are from the United Kingdom"
+  puts "You are from the United Kingdom."
 
   puts "What is your age?"
   age = gets.chomp.to_i
 
   if age < 15
-    puts "You can do nothing"
+    puts "You can do nothing!"
     elsif age.between?(16, 17)
       puts "Are your parents there?(yes/no)"
       consent = gets.chomp
@@ -14,9 +14,9 @@ def uk_rules(x)
           elsif puts "You cannot drink!!"
         end
     elsif age.between?(18, 24)
-    puts "You can vote and you can also drink alcoholic beverages"
+    puts "You can vote and you can also drink alcoholic beverages."
     elsif age >= 25
-    puts "You can vote, drink alcoholic bevarages and you can also rent a car"
+    puts "You can vote, drink alcoholic bevarages and you can also rent a car."
   else
 
   end
@@ -30,7 +30,7 @@ def uk_rules(x)
   if yn.downcase == "n"
     abort
   else
-    check_age(0, "")
+check_age(0, "")
   end
 
 end
@@ -43,8 +43,8 @@ def check_age(age, country)
     if country.upcase == "UK"
       uk_rules(age)
       elsif country.upcase == "US"
-      puts "You are from the United States of America"
-      else puts "The only supported countries are the United Kingdom and the United States"
+      puts "You are from the United States of America."
+    else puts "The only supported countries are the United Kingdom and the United States."
       check_age(0, "")
 
     end
@@ -54,19 +54,19 @@ def check_age(age, country)
 
 
     if age < 18
-      puts "You can do nothing"
+      puts "You can do nothing!"
       elsif age.between?(18, 20)
-      puts "You can vote and smoke"
+      puts "You can vote and smoke."
       elsif age.between?(21, 24)
-      puts "You can vote and I can also drink alcoholic beverages"
+      puts "You can vote and I can also drink alcoholic beverages."
       elsif age >= 25
-      puts "You can vote, drink alcoholic bevarages and you can also rent a car"
+      puts "You can vote, drink alcoholic bevarages and you can also rent a car."
       else
         break
     end
     puts ""
     puts ""
-    puts "Change countr(y), E(n)d app"
+    puts "Change countr(y), E(n)d app."
 
     yn = gets.chomp
 
