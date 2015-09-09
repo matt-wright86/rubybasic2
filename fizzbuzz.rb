4be615cc1a644d5bcf9855fc73cdb1d1b6602c38
justin richards <richards.justind@gmail.com>
2015-09-08 20:37:43 -0500

    proper functionality

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,9 @@
+=begin
 def fizzbuzz(max)
 
 
  # out creates string, if the string of fizz, fizzbuzz and buzz, everything does
  # not meet the criteria (divisible by 3, 5, or 15 is returned as an integer.
-
-
 
   1.upto(max).each {|i|
     out = ''
@@ -17,3 +16,23 @@ def fizzbuzz(max)
 end
 
 fizzbuzz(100)
+=end
+
+def fizzbuzz(x, y)
+
+  a = (x..y).to_a
+  al = a.each{|i|
+  if i % 5 == 0 and i % 3 == 0
+    puts "fizzbuzz"
+  elsif i % 5 == 0
+    puts "buzz"
+  elsif i % 3 == 0
+      puts "fizz"
+  else
+     puts i
+  end
+}
+
+end
+
+fizzbuzz(1, 15)
